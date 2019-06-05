7352c383c6f06daaa649332ddce4558e21844faf
Govind Sandhu <gc.sandhu@gmail.com>
2019-06-05 02:01:13 +0000

    Created a function to generate a random string of letters and numbers

--- express_server.js
+++ express_server.js
@@ -2,12 +2,31 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+
+
+const generateRandomString = function() {
+
+    let charSet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    let randomNum = () =>
+        Math.floor(Math.random() * charSet.length);
+
+    let randomStr = ""
+
+    for (let i = 0; i < 6; i++) {
+        randomStr += charSet.charAt(randomNum());
+    }
+    return randomStr;
+}
+
 
 // app.get("/", (req, res) => {
 //     res.send("Hello!");
@@ -25,6 +44,15 @@ app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars)
 })
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
     let shortURL = req.params.shortURL
