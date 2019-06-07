4f4f095dab1dd0b0eedbaa7686be8d3b028980fe
govsandhu <gc.sandhu@gmail.com>
2019-06-07 21:00:26 +0000

    Fixed edit/update buttons and short u link functionality

--- express_server.js
+++ express_server.js
@@ -26,8 +26,6 @@ const users = {
     }
 }
 
-
-
 function doesUserNameExist(userEmail) {
     let userExists = false;
     for (let id in users) {
@@ -38,8 +36,6 @@ function doesUserNameExist(userEmail) {
     }
     return userExists
 }
-
-
 
 const generateRandomString = function() {
 
@@ -55,7 +51,7 @@ const generateRandomString = function() {
     return randomStr;
 }
 
-
+// This renders the login page. Template vars is an object that gets passed to the login.ejs page. In this case, it passes the cookie object for the associated client ID.
 app.get('/login', (req, res) => {
     let userIDCookie = req.cookies["user_id"]
     let templateVars = {
@@ -64,6 +60,7 @@ app.get('/login', (req, res) => {
     res.render("login", templateVars)
 })
 
+// This adds logic to the urls page. If a cookie doesn't exist, ie. if the user is not logged in, they will receive a message to do so.
 app.get("/urls", (req, res) => {
     let userIDCookie = req.cookies["user_id"]
     let urlArray = [];
@@ -81,18 +78,18 @@ app.get("/urls", (req, res) => {
             })
           }
         }
-    }
+      }
     }
     urlsForUser(userIDCookie);
     let templateVars = {
         userObject: users[userIDCookie],
         urls: urlArray,
     };
-    console.log(urlArray)
 
     res.render("urls_index", templateVars)
 })
 
+// This renders the register page and passes the object of the associated cookie to the register template. In this case, the object should be empty.
 app.get("/register", (req, res) => {
     let userIDCookie = req.cookies["user_id"]
     let templateVars = {
@@ -101,6 +98,8 @@ app.get("/register", (req, res) => {
     };
     res.render("register", templateVars)
 })
+
+// This retrieves inputs by the user from the register page. If the fields are empty, or if the username doesn't exit, it will throw an error. If it passes those two criteria points, the name will be registered and added to the users object.
 app.post('/register', (req, res) => {
     const userEmail = req.body.email;
     const password = req.body.password
@@ -119,25 +118,40 @@ app.post('/register', (req, res) => {
         res.cookie('user_id', randomUserID);
         res.redirect('/urls');
     }
-    console.log(users)
 });
 
 
-
+// This allows the user to delete an existing url if the user is logged in.
 app.post('/urls/:shortURL/delete', (req, res) => {
-    delete urlDatabase[req.params.shortURL];
-    res.redirect('/urls');
+    let userIDCookie = req.cookies["user_id"];
+    let shortURL = req.params.shortURL
+    console.log(userIDCookie)
+      if (urlDatabase[shortURL]) {
+        if (urlDatabase[shortURL].user_id === userIDCookie) {
+            delete urlDatabase[req.params.shortURL];
+            res.redirect('/urls');
+        } else {
+          res.status(403).send("Please login")
+        }
+      } else {
+        res.status(404).send("URL not found")
+      }
 });
 
+//This allows a user to update an existing url from the current shortURL.
 app.post('/urls/:shortURL', (req, res) => {
+    let userIDCookie = req.cookies["user_id"]
     const updatedLongURL = req.body.updatedLongURL;
     const shortURL = req.params.shortURL;
-    if (updatedLongURL) {
-        urlDatabase[shortURL] = updatedLongURL
-    }
+      if (urlDatabase[shortURL]) {
+        if (urlDatabase[shortURL].user_id === userIDCookie) {
+          urlDatabase[shortURL].longURL = updatedLongURL
+        }
+      }
     res.redirect('/urls');
 })
 
+// This allows the user to create a new url.
 app.get("/urls/new", (req, res) => {
     let userIDCookie = req.cookies["user_id"]
     let templateVars = {
@@ -150,33 +164,33 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+//Can this be removed? Should the user be able to create a url from the urls page? The fields are currently missing.
 app.post("/urls", (req, res) => {
     let newLongURL = req.body.longURL
-
-
     let newShortURL = generateRandomString()
     urlDatabase[newShortURL] = newLongURL
-
     res.redirect(`/urls/${newShortURL}`)
 });
 
+//This shows the user the current url selected
 app.get("/urls/:shortURL", (req, res) => {
     let userIDCookie = req.cookies["user_id"]
     let shortURL = req.params.shortURL
     let templateVars = {
         userObject: users[userIDCookie],
         shortURL: req.params.shortURL,
-        longURL: urlDatabase[shortURL]
+        longURL: urlDatabase[shortURL].longURL
     };
     res.render("urls_show", templateVars);
 });
 
+//This redirects the user to the long url
 app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL]
+    const longURL = urlDatabase[req.params.shortURL].longURL
     res.redirect(longURL);
 });
 
-
+//This verifies if the email/password entered are correct, and if not, sends a message that there is an error.
 app.post("/login", (req, res) => {
     let userEmail = req.body.email;
     let enteredPassword = req.body.password;
@@ -192,6 +206,7 @@ app.post("/login", (req, res) => {
       res.redirect('/login')
 })
 
+//This clears the cookie and logs the user out.
 app.post("/logout", (req, res) => {
     res.clearCookie('user_id');
     res.redirect('/urls');
